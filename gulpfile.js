23394f695221cee31a5b5b85b04452277a9a8231
Aleksey Denisov <alde.owl@gmail.com>
2018-11-24 17:29:15 +0700

    -add cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano')
+
+gulp.task('build', ['js', 'css']);
 
 gulp.task('js', () => {
     return gulp.src('scripts/*js')
-        .pipe(concat())
+        .pipe(concat('index.js'))
         .pipe(babel({
             presets: ['@babel/env']
         }))
@@ -14,6 +17,8 @@ gulp.task('js', () => {
 });
 
 gulp.task('css', () => {
-    return gulp.src('*css')
+    return gulp.src('css/*css')
+        .pipe(concat('index.css'))
+        .pipe(cssnano())
         .pipe(gulp.dest('prod/css'));
 });
