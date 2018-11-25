5e67b488be6d5d20ee3ca61298e3ae46c0ab0efb
Aleksey Denisov <alde.owl@gmail.com>
2018-11-25 17:02:14 +0700

    add sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,30 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cssnano = require('gulp-cssnano')
+const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
 
 gulp.task('build', ['js', 'css']);
 
 gulp.task('js', () => {
     return gulp.src('scripts/*js')
-        .pipe(concat('index.js'))
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
+        .pipe(sourcemaps.init())
+            .pipe(concat('index.js'))
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('prod/scripts'));
 });
 
 gulp.task('css', () => {
     return gulp.src('css/*css')
-        .pipe(concat('index.css'))
-        .pipe(cssnano())
+        .pipe(sourcemaps.init())
+            .pipe(concat('index.css'))
+            .pipe(cssnano())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('prod/css'));
 });
